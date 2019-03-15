8a8cbb62b42622315c9ace9ae399466c336b4c6a
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-03-15 23:48:20 +0000

    Add more pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,21 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addNunjucksFilter("datesort", function(obj) {
+        const newObj = {};
+
+        Object.keys(obj)
+            .sort()
+            .reverse()
+            .forEach((key) => {
+                newObj[key] = obj[key];
+            });
+
+        return newObj;
+    });
+
+     eleventyConfig.addNunjucksFilter("objfirst", function (obj) {
+         return obj[Object.keys(obj)[0]];
+     });
+
     return {
         templateFormats: [
             "njk"
@@ -13,7 +30,7 @@ module.exports = function(eleventyConfig) {
             input: "_pages",
             includes: "../_includes",
             data: "../_data",
-            output: "../_build"
+            output: "_build"
         }
     }
 }
