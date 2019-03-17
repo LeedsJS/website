4b352c65a25254ccf34e91f4eeae56248d668cf7
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-03-17 12:43:13 +0000

    Add ti.to tickets

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addNunjucksFilter("datesort", function(obj) {
         const newObj = {};
@@ -15,6 +17,11 @@ module.exports = function(eleventyConfig) {
      eleventyConfig.addNunjucksFilter("objfirst", function (obj) {
          return obj[Object.keys(obj)[0]];
      });
+
+    const today = moment();
+    eleventyConfig.addNunjucksFilter("isfutureevent", function (date) {
+        return today.isSameOrBefore(date, 'day');
+    });
 
     return {
         templateFormats: [
