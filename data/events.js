6c9adb69aa0d0bed13d1488d372a328d044752f0
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-03-30 00:30:37 +0000

    Add streaming pages and minor refactor of event data

--- data/events.js
+++ data/events.js
@@ -1,6 +1,7 @@
 module.exports = () => {
-    return {
-        "2019-02-27": {
+    const data = [
+        {
+            "id": "2019-02-27",
             "title": "testy test test",
             "short_blurb": "this is a test, there's nothing really here",
             "blurb": "this is a test, there's nothing really here",
@@ -18,7 +19,8 @@ module.exports = () => {
             "ticket_date": "2019-02-20",
             "announce_date": "2019-02-01"
         },
-        "2019-01-27": {
+        {
+            "id": "2019-01-27",
             "title": "testy test test",
             "short_blurb": "this is a test, there's nothing really here",
             "blurb": "this is a test, there's nothing really here",
@@ -36,7 +38,8 @@ module.exports = () => {
             "ticket_date": "2019-01-20",
             "announce_date": "2019-01-01"
         },
-        "2019-03-27": {
+        {
+            "id": "2019-03-27",
             "title": "testy test test",
             "short_blurb": "this is a test, there's nothing really here",
             "blurb": "this is a test, there's nothing really here",
@@ -55,5 +58,14 @@ module.exports = () => {
             "ticket_date": "2019-03-27",
             "announce_date": "2019-03-27"
         }
+    ];
+
+    const nextEvent = data.sort((a, b) => {
+        return a.date < b.date;
+    })[0];
+
+    return {
+        data,
+        next_event: nextEvent
     };
 }
