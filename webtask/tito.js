2a344f2d1898cb67038abf73e3763409cbbcfc60
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-03-30 22:31:36 +0000

    Add tito webtask

---
+++ webtask/tito.js
@@ -0,0 +1,93 @@
+const https = require('https');
+const moment = require('moment-timezone');
+
+module.exports = function (context, cb) {
+
+    const getOptions = (path, method) => {
+        return {
+            host: `api.tito.io`,
+            path: `/v3/leedsjs/${path}`,
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                Authorization: `Token token=${context.secrets.tito_key}`,
+            },
+            method: method
+        }
+    };
+
+    makeRequest({
+        host: 'leedsjs.com',
+        path: '/automation/next-event.json',
+        method: 'GET',
+    }, null, (eventData) => {
+        eventData = JSON.parse(eventData);
+
+        const today = moment().tz('Europe/London');
+
+        if (today.isSame(eventData.announce_date, 'day')) {
+            console.log(`It's announcement day, create the tickets!`);
+
+            makeRequest(getOptions('events', 'POST'), {
+                title: eventData.title,
+                slug: eventData.id,
+            }, () => {
+                console.log('Event created');
+                const ticketRelease = moment.tz(eventData.ticket_date, 'Europe/London')
+                    .hour(10).minute(0).toISOString();
+                const ticketEnd = moment.tz(eventData.date, 'Europe/London')
+                    .hour(18).minute(30).toISOString();
+
+                makeRequest(getOptions(`${eventData.id}/releases`, 'POST'), {
+                    title: 'Attendee',
+                    quantity: 74,
+                    start_at: ticketRelease,
+                    end_at: ticketEnd,
+                    price: 0,
+                    waiting_list_enabled_during_sold_out: true
+                }, () => {
+                    console.log('Tickets created')
+                    const startTimeParts = eventData.start_time.split(':');
+                    const startDate = moment.tz(eventData.date, 'Europe/London')
+                        .hour(startTimeParts[0]).minute(startTimeParts[1]).toISOString();
+
+                    const endTimeParts = eventData.end_time.split(':');
+                    const endDate = moment.tz(eventData.date, 'Europe/London')
+                        .hour(endTimeParts[0]).minute(endTimeParts[1]).toISOString();
+
+                    makeRequest(getOptions(`${eventData.id}`, 'PATCH'), {
+                        live: true,
+                        test_mode: false,
+                        start_date: startDate,
+                        end_date: endDate,
+                        description: eventData.blurb
+                    }, () => {
+                        console.log('Tickets live')
+                    });
+                });
+            });
+        } else {
+            cb(null, {});
+        }
+    });
+};
+
+function makeRequest(options, body, cb) {
+    const req = https.request(options, (res) => {
+        let data = '';
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            cb(data);
+        });
+    });
+
+    if (body) {
+        console.log(body);
+        req.write(JSON.stringify(body));
+    }
+
+    req.end();
+}
