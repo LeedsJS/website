72cc3183969786964ab23a513b8aab513a454e54
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2019-04-26 17:29:40 +0100

    Add html minification

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const moment = require('moment-timezone');
 const markdownIt = require('markdown-it');
 const cheerio = require('cheerio');
 const purifycss = require('purify-css');
+const htmlminifier = require('html-minifier');
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("site/img");
@@ -100,6 +101,25 @@ module.exports = function(eleventyConfig) {
                     resolve(content.replace('<link rel="stylesheet" href="/styles.css">', `<style>${css}</style>`));
                 });
             });
+        };
+
+        return content;
+    });
+    eleventyConfig.addTransform('htmlminifier', async function (content, outputPath) {
+        if (outputPath.endsWith(".html")) {
+            return htmlminifier.minify(content, {
+                collapseWhitespace: true,
+                minifyCSS: true,
+                minifyJS: true,
+                removeAttributeQuotes: true,
+                removeComments: true,
+                removeEmptyAttributes: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                sortAttributes: true,
+                sortClassName: true
+            })
         };
 
         return content;
