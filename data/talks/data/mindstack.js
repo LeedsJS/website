e5424633f2d24d7b8cc5f3350e9004e4fea20823
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2019-05-10 15:33:25 +0100

    Move talks into separate files

---
+++ data/talks/data/mindstack.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+    return {
+        "title": "Mindstack",
+        "speaker": "wade-penistone",
+        "abstract": `Bots, machine learning and AI are hot topics in the industry at the moment. The skillsets are in demand and there is plenty of resources for learning. Lots of people now also use voice assistants and helpdesk automated facilities.
+
+However developing for these is still a very vague and improvised strategy, with tools and frameworks only offering a limited scope and most of us as web developers unaware on the layers of compexity involved in creating an effective AI.
+
+Join me for what I believe is a solution to this problem, aimed at simplifying and consolidating the code needed in an nice and easy to use package/ecosystem.`,
+        "date": "2019-02-27",
+        "youtube_video_id": "RA4eGo5uFvE"
+    };
+}
---
+++ data/talks/data/styling-components-on-the-web.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    return {
+        "title": "Styling The Web: With CSS And JS",
+        "speaker": "umair-ahmed",
+        "abstract": `Without styling, the world wide web would just be a load of boxes and plain text. That might have worked a couple of decades ago, but today we want more.
+
+This is why CSS is great, as it makes it easy to create beautiful sites and apps. However, as time has passed and the complexity of app creation has increased, CSS has become harder to tame.
+
+But all is not lost. In this talk we’ll explore some of the problems that are harder to tackle with plain CSS, and I’ll demonstrate how, when merged with JS, you can make things that little bit less complex and easier to maintain.`,
+        "date": "2019-05-29"
+    };
+}
