221e677e12defa890b3116dc75439387f854fc9f
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-05-10 22:34:13 +0100

    Move speakers to new format

---
+++ data/speakers/data/sam-beckham.js
@@ -0,0 +1,17 @@
+module.exports = () => {
+    return {
+        "name": "Sam Beckham",
+        "bio": `Sam is a, front-end developer at GitLab, and co-organiser of Frontend NE. He’s an advocate of pattern libraries and componentising elements; and has given talks on both topics at several events and conferences in the past.
+
+You can follow him as samdbeckham on Github, Twitter, Codepen, or Medium.
+
+Yes, the D stands for David; and no, there’s no relation (unfortunately).`,
+        "picture": "sam-beckham.jpg",
+        "twitter": "samdbeckham",
+        "links": {
+            "Website": "https://sam.beckham.io",
+            "GitHub": "https://github.com/samdbeckham",
+            "Codepen": "https://codepen.io/samdbeckham"
+        }
+    };
+}
