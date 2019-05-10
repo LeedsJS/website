daa013bc96f07d5a9733da1c648b5c3b865c73bf
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2019-05-10 15:33:25 +0100

    Move talks into separate files

---
+++ data/talks/data/coding-is-serious-business.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+    return {
+        "title": "Coding Is Serious Business",
+        "speaker": "luke-bonaccorsi",
+        "abstract": `Coding is serious business. Except it isn't. Code gives us the power to unleash our creative potential in new and interesting ways, whether that’s making interactive art, creating live music and visualisations, building games or even adding joyful flourishes on a web app.
+
+In this talk, I’ll show that our code doesn’t just have to be for building boring applications. Using real examples, I’ll talk about some ways that we can bring creativity into our coding projects, as well as ways for us to share our knowledge and why it’s important that we do. By the end, I hope to have inspired you to bring creativity into your projects and given you the enthusiasm to share your knowledge.`,
+        "date": "2019-02-27",
+        "youtube_video_id": "CWiiKljO7D0"
+    };
+}
---
+++ data/talks/data/what-the-jamstack.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+    return {
+        "title": "What the JAMstack?",
+        "speaker": "jamie-bradley",
+        "abstract": `In this talk I will cover the basics of JAMstack i.e. what it is, why it's becoming so popular and also some of the potential drawbacks with using JAMstack.
+
+I'll then follow up with a short demonstration of a project using Gatsby and showcase how this fits into the JAMstack and why it is proving to be a success. This may be the kick you need to start a blog in 2019!`,
+        "date": "2019-01-30",
+        "youtube_video_id": "s6zjiMvZZ4A"
+    };
+}
