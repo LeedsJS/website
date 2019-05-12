7574820f81d7a2e922f2ba406d3d60c0001947dd
Gideon Caspi <gideoncaspi@gmail.com>
2019-05-12 17:36:56 +0100

    Fix preload link, include fonts and back to top animation

--- site/script.js
+++ site/script.js
@@ -86,9 +86,17 @@ function initGoToTopBtn() {
         }
     }
 
+    function scrollToTop() {
+        const c = document.documentElement.scrollTop || document.body.scrollTop;
+        if (c > 0) {
+          window.requestAnimationFrame(scrollToTop);
+          window.scrollTo(0, c - c / 8);
+        }
+    };
+
     function backToTop() {
         if (window.pageYOffset > 0) {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
+            scrollToTop();
         }
     }
 
