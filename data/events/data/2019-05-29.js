858a5f407a0f4d8b9cb033faa4ae12c6976335f6
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2019-05-22 10:28:11 +0100

    Add AND Digital

--- data/events/data/2019-05-29.js
+++ data/events/data/2019-05-29.js
@@ -4,7 +4,7 @@ module.exports = () => {
         "title": "May - Making Components Pretty and the Life and Times of the Browser",
         "blurb": `This month we have a couple of excellent sounding talks from Andy Carter and Umair Ahmed where we'll be delving into the fascinating history of the web browser and learning about CSS-in-JS!
 
-We'll also be giving away 2 codes for a year subscription for a Jetbrains toolbox product of your choice!`,
+We'll also be giving away 2 codes for a year subscription for a Jetbrains toolbox product of your choice and a year subscription for egghead.io!`,
         "talks": [
             "the-dramatic-history-of-the-web-browser",
             "styling-components-on-the-web"
@@ -13,6 +13,7 @@ We'll also be giving away 2 codes for a year subscription for a Jetbrains toolbo
             "sky-betting-and-gaming",
             "bruntwood",
             "jetbrains",
+            "and-digital"
         ],
         "date": "2019-05-29",
         "start_time": "18:30",
