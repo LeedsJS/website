7bc5db4de5b368d9550bce2f517c5505211f88bf
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2019-05-29 15:04:08 +0100

    Add colin wren and initial June event data

---
+++ data/events/data/2019-06-26.js
@@ -0,0 +1,22 @@
+module.exports = () => {
+    return {
+        "id": "2019-06-26",
+        "title": "June - A Testing Extravaganza!",
+        "blurb": `This month is a testing extravaganza with talks from Colin Wren talking about testing Kafka and Daniel Irvine talking about doing test driven development with React.
+
+We'll also be giving away 2 codes for a year subscription for a Jetbrains toolbox product of your choice!`,
+        "talks": [
+            "testing-kafka-with-jest-and-nodejs"
+        ],
+        "sponsors": [
+            "sky-betting-and-gaming",
+            "bruntwood",
+            "jetbrains",
+        ],
+        "date": "2019-06-26",
+        "start_time": "18:30",
+        "end_time": "20:30",
+        "ticket_date": "2019-06-19",
+        "announce_date": "2019-06-03"
+    };
+}
