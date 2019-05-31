657cd536672216013902e9876297d6f1c04da4ff
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2019-05-31 15:27:02 +0100

    Add videos for May talks and correct symbols in daniel's talk

--- data/talks/data/styling-components-on-the-web.js
+++ data/talks/data/styling-components-on-the-web.js
@@ -7,6 +7,7 @@ module.exports = () => {
 This is why CSS is great, as it makes it easy to create beautiful sites and apps. However, as time has passed and the complexity of app creation has increased, CSS has become harder to tame.
 
 But all is not lost. In this talk we’ll explore some of the problems that are harder to tackle with plain CSS, and I’ll demonstrate how, when merged with JS, you can make things that little bit less complex and easier to maintain.`,
-        "date": "2019-05-29"
+        "date": "2019-05-29",
+        "youtube_video_id": "mJlcQXZfRYw"
     };
 }
