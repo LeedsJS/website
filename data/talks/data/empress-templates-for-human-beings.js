2d0ef82baa1d0d7355d08b054c2243cac4ff987f
Chris Manson <chris@manson.ie>
2019-07-01 21:55:02 +0100

    Adding chris' talk

---
+++ data/talks/data/empress-templates-for-human-beings.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    return {
+      "title": "Empress - JAM Stack Templates for Human Beings",
+      "speaker": "chris-manson",
+      "abstract": `With the metoric rise of the JAM Stack it is easy to get lost and not know where to start. And with JSX, React, GraphQL, Vue, and all the other technologies floating around it's easy to think 'What ever happened to good old HTML!?.
+
+Empress is a new project that is trying to bring a bit of clarity to the market of static site generation and hopes to focus on the end-user, both the people wanting to make websites and developers or designers that want to make templates.
+
+Chris will walk you through how Empress is different to Gatsby and VuePress, explain what an "Empress product" is and hopefully inspire you to build your own empress-blog template even if you only know HTML and CSS!`,
+      "date": "2019-07-31"
+    }
+}
