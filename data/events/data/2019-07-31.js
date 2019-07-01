af6fc623313633877e566997f1d2baab27308eac
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-07-01 22:56:23 +0100

    Add July event

---
+++ data/events/data/2019-07-31.js
@@ -0,0 +1,24 @@
+module.exports = () => {
+    return {
+      id: "2019-07-31",
+      title: "July - Fighting the Imposter Goblin and JAM for humans",
+      blurb: `This month we have Beth North talking about fighting the Imposter Goblin and our old friend (and former organiser) Chris Manson is visiting us to talk about Empress!
+
+We'll also be giving away 2 codes for a year subscription for a Jetbrains toolbox product of your choice!`,
+      talks: [
+        "imposter-syndrome-as-a-techie",
+        "empress-templates-for-human-beings"
+      ],
+      sponsors: [
+        "sky-betting-and-gaming",
+        "bruntwood",
+        "starlight-software",
+        "jetbrains"
+      ],
+      date: "2019-07-31",
+      start_time: "18:45",
+      end_time: "20:30",
+      ticket_date: "2019-07-24",
+      announce_date: "2019-07-02"
+    };
+}
