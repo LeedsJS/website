34678edc62a4afe5a50a329cc573a747a7c054b9
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2019-08-05 13:30:07 +0100

    Add August event

---
+++ data/speakers/data/phil-hawksworth.js
@@ -0,0 +1,15 @@
+module.exports = () => {
+    return {
+      name: "Phil Hawksworth",
+      bio: `Phil is Principal Developer Advocate at Netlify, the fastest growing automation and hosting platform for modern websites.
+
+With a passion for browser technologies, and the empowering properties of the Web, he loves seeking out ingenuity and simplicity, especially in places where over-engineering is common.
+
+Phil’s 20 year career in web development includes time as a Software Engineer at Verisign, an Open Source Evangelist at British Telecom, and Technology Director at R/GA where he worked with clients around the world such as Nike, Google, Beats By Dre and Samsung to bring engaging and effective experiences to the widest audience possible.`,
+      picture: "phil-hawksworth.jpg",
+      twitter: "philhawksworth",
+      links: {
+        Website: "https://www.hawksworx.com/"
+      }
+    };
+}
---
+++ data/talks/data/are-you-being-servered-exploring-a-serverless-web.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    return {
+      title: "Are you being servered? — Exploring a serverless web",
+      speaker: "phil-hawksworth",
+      abstract: `“JAMstack” is an emerging term which some may like and others might not. It describes an approach to architecting and delivering sites as pre-rendered assets, and where necessary, enhancing them with JavaScript and services via APIs… an approach which some may like and others might not.
+
+Personally I love this approach. And I want to explain why.
+
+There has never been a better time for a fresh look at how we approach building and delivering web sites and applications. From the careful selection of JavaScript libraries and frameworks, to the use of emerging tools, services, and platforms.`,
+      date: "2019-08-28"
+    };
+}
