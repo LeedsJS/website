3d8531624cf35fc97583400c9a952d34f3cbb9a3
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-09-02 21:11:10 +0100

    Add Katie Fenn, talk and september 2019 event

---
+++ data/speakers/data/katie-fenn.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    return {
+        name: "Katie Fenn",
+        bio: "Katie Fenn is a software engineer from Sheffield working at Monzo. She loves attending conferences, writing talks and building nifty things with the Web. When not at work, you'll most likely find her on a bike in the Peak District National Park.",
+        pronouns: "She/Her",
+        picture: "katie-fenn.jpg",
+        twitter: "katie_fenn",
+        links: {
+            "Front End North": "https://frontendnorth.com/"
+        }
+    }
+}
---
+++ data/sponsors/data/dotjs.js
@@ -0,0 +1,8 @@
+module.exports = () => {
+    return {
+        name: "dotJS    ",
+        url: "https://www.dotjs.io/",
+        logo: "dotjs.png",
+        twitter: "dotJS"
+    }
+}
