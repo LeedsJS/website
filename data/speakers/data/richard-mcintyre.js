a02009f95b590d1607dfdaa27cd588eae99a910a
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-09-04 21:17:34 +0100

    Add Richard McIntyre talk

---
+++ data/speakers/data/richard-mcintyre.js
@@ -0,0 +1,10 @@
+module.exports = () => {
+    return {
+        name: "Richard McIntyre",
+        bio: "Rich Mcintyre is a straight talking internet enthusiast, currently working as a Software Engineering Manager at DAZN. Working with teams in Amsterdam and Leeds.",
+        pronouns: "He/Him",
+        picture: "richard-mcintyre.jpg",
+        twitter: "mackstar",
+        links: {}
+    }
+}
