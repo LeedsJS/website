d99fe0225efdd4cfbcf254bd05c8618859740361
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-09-05 08:01:09 +0100

    Text tweak

--- data/talks/data/scaling-frontends-on-sports-websites.js
+++ data/talks/data/scaling-frontends-on-sports-websites.js
@@ -2,7 +2,7 @@ module.exports = () => {
     return {
         title: "Scaling Frontends on Sports Websites",
         speaker: "richard-mcintyre",
-        abstract: "Sports websites have come a long way and so have the technologies that power their frontends. Rich worked at the BBC Sport on their Olympics and famous 'yellow' sports site was being developed and now works at DAZN where focus has shifted to Micro-Frontends. Find out more about how this evolution in frontend architecture affect how you might approach your next project that needs to scale.",
+        abstract: "Sports websites have come a long way and so have the technologies that power their frontends. Rich worked at the BBC Sport on their Olympics and when the famous 'yellow' sports site was being developed and now works at DAZN where focus has shifted to Micro-Frontends. Find out more about how this evolution in frontend architecture affects how you might approach your next project that needs to scale.",
         date: "2019-09-25"
     }
 }
