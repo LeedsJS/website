e0bc4a48aae374da46eb8190dd8353b3a2864a68
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-12-03 21:48:34 +0000

    Add conferences email

---
+++ data/comms/data/2019-12-04.js
@@ -0,0 +1,25 @@
+module.exports = () => {
+    return {
+        id: "2019-12-04",
+        title: "Conferences, conferences, conferences!",
+        tweet: "Our friends at @FrontEndSheff are organising @frontendnorth, and we're partnering with the @thejsnation and @ReactAmsterdam conferences!",
+        body: `Hi folks!
+        
+Just a quick message to tell you about a few conference things from our friends and partners!
+
+## Front End North
+Our friends at [Front End Sheffield](https://www.meetup.com/Front-End-Sheffield/) are running the Front End North conference on February 7th 2020! They've only announced their keynote speakers at the moment, but already the line-up is looking fantastic!
+
+They have tickets for sale at the incredibly reasonable price of £50 and you can grab yours [over at the website](https://frontendnorth.com/).
+
+## React Summit and JSNation
+We've partnered with the React Summit and JSNation conferences and will be giving away tickets and sharing discount codes in the new year. In the meantime, they're looking for talk submissions and you can find the links below:
+
+[React Summit talk submission](https://docs.google.com/forms/d/e/1FAIpQLSf1gJZym7y79DcqFyG6sZ6-uoxDBnczD0pI7JGcXySwVZ6MfQ/viewform)
+
+[JSNation talk submission](https://docs.google.com/forms/d/e/1FAIpQLScfRsnB8Qv3JRHVc9Z6j0Ztc7O1jTeUbTnxLvQ9dtpmNneA1Q/viewform)
+
+See you in the new year!`,
+        date: "2019-12-04"
+    };
+}
