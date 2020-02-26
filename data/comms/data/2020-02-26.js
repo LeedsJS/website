d45c9d4405034f821fa98bd685d97abbcffa09cd
Luke Bonaccorsi <luke.bonaccorsi@and.digital>
2020-02-26 11:10:47 +0000

    Add last minute lineup change comm

---
+++ data/comms/data/2020-02-26.js
@@ -0,0 +1,16 @@
+module.exports = () => {
+  return {
+    id: "2020-02-26",
+    title: "Last minute line-up change!",
+    tweet:
+      "Unfortunately due to illness, one of tonight's talks can't go ahead. Thankfully @CodeFoodPixels is filling in with a talk about home automation with JavaScript!",
+    body: `Hi folks!
+
+Due to illness and other reasons, the speakers for "How to save time, budget and your in-house development teams" have had to drop out of tonight's event.
+
+Thankfully, our own Luke Bonaccorsi is filling in with his talk "How I ended up automating my curtains and shouting at my laptop"!
+
+You can find more details on the event page: [https://leedsjs.com/events/2020-02-26/](https://leedsjs.com/events/2020-02-26/)`,
+    date: "2020-02-26"
+  };
+};
