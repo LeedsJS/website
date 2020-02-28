c0a0b28b8dca450d68048048331f3c481bc8d186
Luke Bonaccorsi <luke.bonaccorsi@and.digital>
2020-02-28 15:49:23 +0000

    Fix issue with form data grabbing for feedback form

--- site/script.js
+++ site/script.js
@@ -1,143 +1,150 @@
 function jsonp(url, data, callback) {
-    const src = url + (url.indexOf('?') > -1 ? '&' : '?');
-    const head = document.querySelector('head');
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
+  const src = url + (url.indexOf("?") > -1 ? "&" : "?");
+  const head = document.querySelector("head");
+  const script = document.createElement("script");
+  script.type = "text/javascript";
 
-    window.jsonpCallback = (data) => {
-        head.removeChild(script);
-        callback(data);
-    };
+  window.jsonpCallback = data => {
+    head.removeChild(script);
+    callback(data);
+  };
 
-    data.c = 'jsonpCallback';
+  data.c = "jsonpCallback";
 
-    const params = [];
+  const params = [];
 
-    Object.keys(data).forEach((param) => {
-        params.push(param + '=' + encodeURIComponent(data[param]))
-    });
+  Object.keys(data).forEach(param => {
+    params.push(param + "=" + encodeURIComponent(data[param]));
+  });
 
-    script.src = src + params.join('&');
+  script.src = src + params.join("&");
 
-    head.appendChild(script);
+  head.appendChild(script);
 }
 
 function getFormData(form) {
-    const data = {};
+  const data = {};
 
-    Array.prototype.forEach.call(form.elements, (el) => {
-        if (el.name.length > 0 && el.value.length > 0) {
-            data[el.name] = el.value;
-        }
-    });
+  Array.prototype.forEach.call(form.elements, el => {
+    if (el.type === "checkbox" && el.checked === true) {
+      data[el.name] = el.value;
+    } else if (el.name.length > 0 && el.value.length > 0) {
+      data[el.name] = el.value;
+    }
+  });
 
-    return data;
+  return data;
 }
 
 function installMailchimp() {
-    const form = document.querySelector('#mailchimp-form');
-   
-    if (form) {
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
+  const form = document.querySelector("#mailchimp-form");
 
-            const data = getFormData(form);
-            const url = form.action.replace('/post?', '/post-json?');
+  if (form) {
+    form.addEventListener("submit", event => {
+      event.preventDefault();
 
-            jsonp(url, data, (data) => {
-                const response = document.querySelector('#mailchimp-response');
+      const data = getFormData(form);
+      const url = form.action.replace("/post?", "/post-json?");
 
-                response.classList.remove('hidden', 'success', 'error');
+      jsonp(url, data, data => {
+        const response = document.querySelector("#mailchimp-response");
 
-                if (data && data.result === 'success') {
-                    response.classList.add('success');
-                    response.innerHTML = data.msg;
-                } else if (data && data.result === 'error') {
-                    response.classList.add('error');
-                    response.innerHTML = data.msg;
-                } else {
-                    response.classList.add('error');
-                    response.innerHTML = "There was an unexpected error when submitting, please try again later";
-                }
-            });
-        });
-    }
+        response.classList.remove("hidden", "success", "error");
+
+        if (data && data.result === "success") {
+          response.classList.add("success");
+          response.innerHTML = data.msg;
+        } else if (data && data.result === "error") {
+          response.classList.add("error");
+          response.innerHTML = data.msg;
+        } else {
+          response.classList.add("error");
+          response.innerHTML =
+            "There was an unexpected error when submitting, please try again later";
+        }
+      });
+    });
+  }
 }
 
 function installPrizeDraw() {
-    const form = document.querySelector('#prize-draw-form');
-    
-    if (form) {
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
+  const form = document.querySelector("#prize-draw-form");
 
-            const data = getFormData(form);
-            const url = form.action;
+  if (form) {
+    form.addEventListener("submit", event => {
+      event.preventDefault();
 
-            jsonp(url, data, (data) => {
-                const response = document.querySelector('#prize-draw-response');
+      const data = getFormData(form);
+      const url = form.action;
 
-                response.classList.remove('hidden', 'success', 'error');
-                
-                if (data && data.message) {
-                    response.classList.add('success');
-                    response.innerHTML = data.message;
-                } else {
-                    response.classList.add('error');
-                    response.innerHTML = "There was an unexpected error when submitting, please try again later";
-                }
-            });
-        });
-    }
+      jsonp(url, data, data => {
+        const response = document.querySelector("#prize-draw-response");
+
+        response.classList.remove("hidden", "success", "error");
+
+        if (data && data.message) {
+          response.classList.add("success");
+          response.innerHTML = data.message;
+        } else {
+          response.classList.add("error");
+          response.innerHTML =
+            "There was an unexpected error when submitting, please try again later";
+        }
+      });
+    });
+  }
 }
 
 function lazyLoad() {
-    Array.prototype.forEach.call(document.querySelectorAll('div[data-type="lazy"]'), (el) => {
-        if ((el.getBoundingClientRect().top - window.innerHeight) <= 0) {
-            const newEl = document.createElement(el.dataset.tag);
-            Array.prototype.forEach.call(el.attributes, (attr) => {
-                newEl.setAttribute(attr.name, attr.value);
-            })
-            el.parentNode.replaceChild(newEl, el);
-        }
-    });
+  Array.prototype.forEach.call(
+    document.querySelectorAll('div[data-type="lazy"]'),
+    el => {
+      if (el.getBoundingClientRect().top - window.innerHeight <= 0) {
+        const newEl = document.createElement(el.dataset.tag);
+        Array.prototype.forEach.call(el.attributes, attr => {
+          newEl.setAttribute(attr.name, attr.value);
+        });
+        el.parentNode.replaceChild(newEl, el);
+      }
+    }
+  );
 }
 
 function initGoToTopBtn() {
-    const goTopBtn = document.querySelector('.back-to-top');
+  const goTopBtn = document.querySelector(".back-to-top");
 
-    function trackScroll() {
-        const scrolled = window.pageYOffset;
-        const threshold = 400;
+  function trackScroll() {
+    const scrolled = window.pageYOffset;
+    const threshold = 400;
 
-        if (scrolled > threshold) {
-            goTopBtn.classList.remove('hidden');
-        }
-        if (scrolled < threshold) {
-            goTopBtn.classList.add('hidden');
-        }
+    if (scrolled > threshold) {
+      goTopBtn.classList.remove("hidden");
     }
-
-    function scrollToTop() {
-        const c = document.documentElement.scrollTop || document.body.scrollTop;
-        if (c > 0) {
-          window.requestAnimationFrame(scrollToTop);
-          window.scrollTo(0, c - c / 8);
-        }
-    };
-
-    function backToTop() {
-        if (window.pageYOffset > 0) {
-            scrollToTop();
-        }
+    if (scrolled < threshold) {
+      goTopBtn.classList.add("hidden");
     }
+  }
 
-    window.addEventListener('scroll', trackScroll, { passive: true });
-    goTopBtn.addEventListener('click', backToTop);
-};
+  function scrollToTop() {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, c - c / 8);
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      scrollToTop();
+    }
+  }
+
+  window.addEventListener("scroll", trackScroll, { passive: true });
+  goTopBtn.addEventListener("click", backToTop);
+}
 
 lazyLoad();
 installMailchimp();
 installPrizeDraw();
 initGoToTopBtn();
-window.addEventListener('scroll', lazyLoad, { passive: true })
+window.addEventListener("scroll", lazyLoad, { passive: true });
