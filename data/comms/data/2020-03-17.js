c53cc2bdb23eff315152990dbff4e0cd7e0b2b9e
Luke Bonaccorsi <luke.bonaccorsi@and.digital>
2020-03-17 10:50:05 +0000

    Remove March event and add comm about cancellation

---
+++ data/comms/data/2020-03-17.js
@@ -0,0 +1,19 @@
+module.exports = () => {
+  return {
+    id: "2020-03-17",
+    title: "Our March event is cancelled",
+    tweet: `Due to #covidー19uk, I've made the decision to cancel our March event.
+
+I don't know what the situation will be in April, but I'll be looking into ways to run it online if necessary!`,
+    body: `Hi folks!
+
+Due to the current situation with COVID-19, I've decided to cancel our March event. I'd hate for anyone to get ill because of LeedsJS and it's better to be safe than sorry.
+
+As of right now, I don't know what the situation will be in April. I'm going to assume that it'll likely be a similar or worse situation, but I'll also be looking into how we can run LeedsJS as a fully remote event.
+
+Stay safe!
+
+Luke`,
+    date: "2020-03-17"
+  };
+};
