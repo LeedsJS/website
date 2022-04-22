8ed7ff509e4ed027a36648b61e2e4b85ddd97d33
Rich McIntyre <rich.mcintyre@bjss.com>
2022-04-22 20:20:24 +0100

    Adding agenda.

--- data/events/data/2022-05-17.js
+++ data/events/data/2022-05-17.js
@@ -3,14 +3,21 @@ module.exports = () => {
     id: "2022-05-17",
     title:
       "May 2022 - Scaling up to 250,000 players in a single game with React.js",
-    blurb: "",
+    blurb: `## Agenda:
+    
++ 18:00 Pizza and drinks
++ 18:30 Talk 1: TBC
++ 18:45 Break and maybe another drink
++ 19:00 Talk 2: Scaling up to 250,000 players in a single game with React.js
++ 19:30 Mingling + Sharing of JS News
++ 20:00 Home time`,
     talks: ["scaling-up-to-250,000-players-in-a-single-game-with-reactjs"],
     sponsors: ["parallax"],
     prizes: [],
     date: "2022-05-17",
     start_time: "18:00",
     end_time: "20:00",
-    ticket_date: "2022-04-23",
-    announce_date: "2022-04-23",
+    ticket_date: "2022-04-22",
+    announce_date: "2022-04-22",
   };
 };
