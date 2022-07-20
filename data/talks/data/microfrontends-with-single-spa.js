7bfacf208e513f7641802e0b51c2717a4ce84286
Rich McIntyre <rich.mcintyre@bjss.com>
2022-07-20 10:54:09 +0100

    Adding synopsis for talks

--- data/talks/data/microfrontends-with-single-spa.js
+++ data/talks/data/microfrontends-with-single-spa.js
@@ -2,7 +2,8 @@ module.exports = () => {
   return {
     title: "Micro-Frontends with Single SPA",
     speaker: ["benjamin-collingwood"],
-    abstract: "",
+    abstract:
+      "SingleSPA promises us a simple to use micro-frontend architecture allowing us to use multiple frameworks on one page with no refreshing! However, do Single-SPA's promises bear out in practice?",
     date: "2022-07-27",
   };
 };
