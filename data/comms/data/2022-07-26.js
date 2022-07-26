30db57ceaad09738a357584f5d3e10ee19a83144
Rich McIntyre <rich.mcintyre@bjss.com>
2022-07-26 09:54:45 +0100

    Announcement reminder.

---
+++ data/comms/data/2022-07-26.js
@@ -0,0 +1,17 @@
+module.exports = () => {
+  return {
+    id: "2022-07-26",
+    title: "Our July event is tomorrow!",
+    tweet: `A reminder that our July event is tomorrow. Please signup here: https://leedsjs.com`,
+    body: `Hi folks!
+  
+  Despite rail strikes we will still go ahead with our meetup tomorrow evening. Please signup here if you haven't already: https://leedsjs.com
+
+  For those of you wanting to watch from the comfort of your own home I will send out a Zoom link tomorrow.
+
+  Many thanks,
+  
+  Rich`,
+    date: "2022-07-26",
+  };
+};
