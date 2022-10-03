3695e48b28bc4b1d1f9b066845a31943651c1a30
Rich McIntyre <rich.mcintyre@bjss.com>
2022-10-03 17:12:00 +0100

    adding the next october 2022 user group info.

---
+++ data/events/data/2022-10-19.js
@@ -0,0 +1,16 @@
+module.exports = () => {
+  return {
+    id: "2022-10-19",
+    title: "Interactive Accessibility Quiz",
+    blurb: "",
+    talks: ["interactive-accessibility-quiz"],
+    sponsors: ["bjss"],
+    prizes: [],
+    date: "2022-10-19",
+    start_time: "18:00",
+    end_time: "20:00",
+    venue: "BJSS",
+    ticket_date: "2022-10-04",
+    announce_date: "2022-10-04",
+  };
+};
