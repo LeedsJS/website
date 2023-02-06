9b24ce577d26b77cdf4a10ef3ae33b09e73b52d6
Rich McIntyre <rich.mcintyre@bjss.com>
2023-02-06 15:44:31 +0000

    Feb event reminder

---
+++ data/comms/data/2023-02-07.js
@@ -0,0 +1,19 @@
+module.exports = () => {
+  return {
+    id: "2023-02-07",
+    title: "LeedsJS is tomorrow!",
+    tweet: `LeedsJS is tomorrow! Details here: https://www.meetup.com/leeds-js/events/290738052/`,
+    body: `Hi folks!
+
+    This will be the last message from the old system before moving back entirely to MeetUp. Please sign up [here](https://www.meetup.com/leeds-js/) to stay in touch.
+
+    LeedsJS will be tomorrow (Weds Feb 8th) at 6pm. [Details are here](https://www.meetup.com/leeds-js/events/290738052/). 
+
+    Look forward to seeing you there.
+    
+    Many thanks,
+    
+    Rich`,
+    date: "2023-02-07",
+  };
+};
